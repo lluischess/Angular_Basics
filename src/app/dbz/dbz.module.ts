ff73df69467e9ee67b134aa83dc405c9a7023a09
lluischess <lluischess@gmail.com>
2024-09-24 23:02:08 +0200

    form component DBZ

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { ListaDbzComponent } from './components/lista/lista.component';
+import { FormDbzComponent } from './components/form/form.component';
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    ListaDbzComponent
+    ListaDbzComponent,
+    FormDbzComponent
 
   ],
   imports: [
