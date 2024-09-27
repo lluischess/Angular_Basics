047d17cb5fcaef58d7e5fb027b407a5708968e75
lluischess <lluischess@gmail.com>
2024-09-27 16:29:14 +0200

    creando un servicio de datos

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -8,26 +8,8 @@ import { Character } from '../interfaces/character.interface';
 
 export class MainPageComponent {
     title = 'Main Page';
-    public characters : Character[] = [
-      {
-        name: 'Krillin',
-        power: 100
-      },
-      {
-        name: 'Goku',
-        power: 9000
-      }
-    ];
 
-    public addCharacter(Newcharacter: Character): void {
-      //this.characters.push(character);
-      console.log(Newcharacter);
-      this.characters.push(Newcharacter);
-      console.log(this.characters);
-    }
+    constructor(public dbzService: DbzService) {
 
-    public onDeleteCharacter( index: number ): void {
-      console.log({ index });
-      this.characters.splice(index, 1);
-    }
+     }
 }
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({providedIn: 'root'})
+export class DbzService {
+  public characters : Character[] = [
+    {
+      name: 'Krillin',
+      power: 100
+    },
+    {
+      name: 'Goku',
+      power: 9000
+    }
+  ];
+
+  public addCharacter(Newcharacter: Character): void {
+    //this.characters.push(character);
+    console.log(Newcharacter);
+    this.characters.push(Newcharacter);
+    console.log(this.characters);
+  }
+
+  public onDeleteCharacter( index: number ): void {
+    console.log({ index });
+    this.characters.splice(index, 1);
+  }
+}
