2554ffe95e9b3d46460d1232bb7d63e3b9f2560b
lluischess <lluischess@gmail.com>
2024-09-27 16:42:32 +0200

    instalar package uuid

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({providedIn: 'root'})
 export class DbzService {
