b7ae47bce45c3ce9ea44d6df37533b867a067a62
lluischess <lluischess@gmail.com>
2024-09-27 17:49:05 +0200

    privatizar el servicio

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DbzService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -9,7 +10,17 @@ import { DbzService } from '../services/dbz.service';
 export class MainPageComponent {
     title = 'Main Page';
 
-    constructor(public dbzService: DbzService) {
+    constructor(private dbzService: DbzService) {}
 
-     }
+    get Characters(): Character[] {
+        return [...this.dbzService.characters];
+      }
+
+      onDeleteCharacter(id: string): void {
+        this.dbzService.onDeleteCharacterbyId(id);
+      }
+
+      onaddCharacter(character: Character): void {
+        this.dbzService.addCharacter(character);
+      }
 }
